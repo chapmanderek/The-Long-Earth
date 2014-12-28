fbb6d068bc46778438c3db4fa815139431c13986
Derek Chapman <chapmanderek@gmail.com>
2014-12-27 18:58:20 -1000

    Map class extended to allow for various actions with a unit.  Can now move unit.

--- long-earth.rb
+++ long-earth.rb
@@ -20,37 +20,67 @@ class Map
 
 		0.upto(@rows-1) do |x|
 			0.upto(@cols-1) do |y|
-				if(@map[x][y] == @find_unit)
-					have = true
+				if(@map[x][y] != nil)
+					if(@map[x][y].name == @find_unit)
+						have = true
+					end
 				end
 			end
 		end
-
-
 		return have
+	end
+
+	def unit_location(find_unit)
+		@find_unit = find_unit
+
+		0.upto(@rows-1) do |x|
+			0.upto(@cols-1) do |y|
+				if(@map[x][y] != nil)
+					if(@map[x][y].name == @find_unit)
+						return x, y
+					end
+				end
+			end
+		end
+		return false, false
+	end
+
+	def get_unit(unit_name)
+		@unit_name = unit_name
+		x, y = unit_location(@unit_name)
+		return @map[x][y]
+	end
+
+	def remove_unit(unit_name)
+		@unit_name = unit_name
+		x, y = unit_location(@unit_name)
+		@map[x][y] = nil
 	end
 
 	def print_map()
 		0.upto(@rows-1) do |x|
 			0.upto(@cols-1) do |y|
 				if(@map[x][y] == nil)
-					print "0"
+					print " 0 "
 				else
 					print @map[x][y].name
+					print " "
 				end
 				print "|"
 			end
-			puts
+			puts 
 		end
+		puts "\n \n"
 	end
 end
 
 class Unit
-	attr_reader :movement_speed, :attack_power, :name
+	attr_reader :movement_speed, :attack_power, :name, :player
 	def initialize(args)
 		@name = args['name']
 		@movement_speed = 1
 		@attack_power = 1
+		@player = args['player']
 	end
 end
 
@@ -69,15 +99,15 @@ class Game
 		@map_datum = Map.new({"rows" => 10, "cols" => 10})
 		@map_west = Map.new({"rows" => 20, "cols" => 20})
 
-		a1 = Unit.new({"name" => "A1"})
-		place_unit({"unit" => a1, "x" => 0, "y" => 0, "which_map" => "datum"})
-		a2 = Human.new({"name" => "A2"})
-		place_unit({"unit" => a2, "x" => 0, "y" => 1, "which_map" => "datum"})
-		a3 = Human.new({"name" => "A3"})
-		place_unit({"unit" => a3, "x" => 0, "y" => 2, "which_map" => "datum"})
+		a1 = Unit.new({"name" => "A1", "player" => 1})
+		place_unit_on_map({"unit" => a1, "x" => 0, "y" => 0, "which_map" => "datum"})
+		a2 = Human.new({"name" => "A2", "player" => 1})
+		place_unit_on_map({"unit" => a2, "x" => 0, "y" => 1, "which_map" => "datum"})
+		a3 = Human.new({"name" => "A3", "player" => 1})
+		place_unit_on_map({"unit" => a3, "x" => 0, "y" => 2, "which_map" => "datum"})
 	end
 
-	def place_unit(args)
+	def place_unit_on_map(args)
 		@which_map = args['which_map']
 
 		case @which_map
@@ -103,19 +133,46 @@ class Game
 		end
 	end
 
-	def move_unit(args)
-		@unit = args['unit']
+	def move_unit(args)  #need unit name, new x, new y, new map
+		@unit_name = args['unit_name']
+
+		#determine which map unit is on
+		#get unit from old location --> put into temperorary holder
+		#remove unit from old location
+		if(@map_west.haveunit?(@unit_name))
+			temp_unit_holder = @map_west.get_unit(@unit_name)
+			@map_west.remove_unit(@unit_name)
+		elsif(@map_datum.haveunit?(@unit_name))
+			temp_unit_holder = @map_datum.get_unit(@unit_name)
+			@map_datum.remove_unit(@unit_name)
+		elsif(@map_east.haveunit?(@unit_name))
+			temp_unit_holder = @map_east.get_unit(@unit_name)
+			@map_east.remove_unit(@unit_name)
+		end
+		
+		#put into new locatin
 		@new_x = args['new_x']
 		@new_y = args['new_y']
 		@to_map = args['to_map']
-		place_unit({"unit" => @unit, "x" => @new_x, "y" => @new_y, "which_map" => @new_map})
-
-		#need to get rid of unit being held at old location
+		place_unit_on_map({"unit" => temp_unit_holder, "x" => @new_x, "y" => @new_y, "which_map" => @to_map})
 	end
 
+	def tests()
+		#output should be true --> false --> 0, 2 --> A2
+		puts @map_datum.haveunit?("A1")
+		puts @map_datum.haveunit?("C5")
+		puts @map_datum.unit_location("A3")
+		testunit = @map_datum.get_unit("A2")
+		puts testunit.name
+
+		#output should be datum map, then with datum map and pieces moved around
+		print_map({"which_map" => "datum"})
+		move_unit({"unit_name" => "A1", "new_x" => 2, "new_y" => 2, "to_map" => "datum"})
+		move_unit({"unit_name" => "A2", "new_x" => 4, "new_y" => 5, "to_map" => "datum"})
+		print_map({"which_map" => "datum"})
+	end
 end
 
 
 game = Game.new()
-game.print_map({'which_map' => "datum"})
-#game.move_unit({"unit" => a1, "new_x" => 2, "new_y" => 2, "to_map" => "datum"})
+game.tests()
